999cd7ca04a0824794dd662bdcb975bd0de74bf8
ChyyamakaU <amakaumunna90@gmail.com>
2026-06-29 15:36:10 +0100

    feature: add products and sections

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ let products = [
     { productName: "soft drink", price: 700, section: "groceries" },
     { productName: "biscuit", price: 300, section: "groceries" },
     { productName: "sugar", price: 1500, section: "groceries" },
-    { name: "oranges", price: 700, section: "groceries" },
-    { name: "Gum", price: 200, section: "groceries" }
+    { productName: "oranges", price: 700, section: "groceries" },
+    { productName: "Gum", price: 200, section: "groceries" }
 ]
 
  
-let productDiscount = products.map((product)=>product.price-product.price*0.10)
+let productDiscount = products.map((product)=>{
+    return {
+        ...product,
+        price : product.price-product.price*0.10
+    }
+})
 
 console.log(productDiscount)
