cfa559687bf1127f455df76899ddbda714353320
ChyyamakaU <amakaumunna90@gmail.com>
2026-06-29 14:31:56 +0100

    feature/ initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let products = [
+
+    { productname: "Apple", price: 400, section: "groceries" },
+    { productName: "soft drink", price: 700, section: "groceries" },
+    { productName: "biscuit", price: 300, section: "groceries" },
+    { productName: "sugar", price: 1500, section: "groceries" },
+    { name: "oranges", price: 700, section: "groceries" },
+    { name: "Gum", price: 200, section: "groceries" }
+]
+
+ 
+
