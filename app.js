c118bca04d593450e7b13ef22a9025268d70a58e
Kelvinachir-stack <kelvinachir97@gmail.com>
2026-06-29 14:37:37 +0100

    Add discount calculation feature

---
+++ app.js
@@ -0,0 +1,10 @@
+let product = [
+
+{product: "electric fan", section : "electronics", price : 2000 },
+{product: "electric fan", section : "electronics", price : 2000 },
+{product: "electric fan", section : "electronics", price : 2000 },
+{product: "electric fan", section : "electronics", price : 2000 },
+{product: "electric fan", section : "electronics", price : 2000 }
+
+
+]
